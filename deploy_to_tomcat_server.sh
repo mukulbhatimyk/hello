548ab01d118f530372b3f4ce0f61adf8e2b752c3
chandu2035 <chandu.2035@gmail.com>
2018-12-02 17:33:31 +0400

    deployment script created

---
+++ deploy_to_tomcat_server.sh
@@ -0,0 +1,36 @@
+# Deployment script
+
+TOMCAT_HOME=/opt/apache-tomcat-7.0.92
+TOMCAT_WEB_DIR=$TOMCAT_HOME/webapps
+SOURCE_DIR=./target
+
+echo "Stopping tomcat server"
+sudo bash $TOMCAT_HOME/bin/shutdown.sh > /dev/null 2>&1
+
+if [ $? -eq 0 ];
+ then
+   echo "Tomcat services stopped successfully"
+ else
+   echo "Failed to stop services, please look into the logs"
+fi
+
+echo "Deployment started ......"
+sudo rm -rf Hello*
+sudo cp $SOURCE_DIR/*.war $TOMCAT_WEB_DIR
+
+if [ $? -eq 0 ];
+ then
+   echo "Deployed war file successfully"
+ else
+   echo "Deployment failed!"
+fi
+
+echo "Tomcat server starting ....."
+sudo bash $TOMCAT_HOME/bin/startup.sh > /dev/null 2>&1
+
+if [ $? -eq 0 ];
+ then
+   echo "Tomcat services up and running....."
+ else
+   echo "Failed to start server!, please look into the logs"
+fi
